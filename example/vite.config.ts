d47fda0411b6066a9030ec8a27eabe98820df4b8
Gabriel Petersson <gabriielpetersson@gmail.com>
2023-04-30 20:15:39 -0700

    inline sourcemap

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { sourcemap: true },
+  build: { sourcemap: "inline" },
 });
